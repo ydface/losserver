e5d60603889f56c0a18a69ae6be1623875da6749
Ydface <329302080@qq.com>
2014-09-24 19:17:03 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+__author__ = 'Ydface'
+
+
+import server
+
+
+def main():
+    server.start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+__author__ = 'Ydface'
+
+import sys
+from twisted.internet.protocol import ServerFactory
+from twisted.protocols.basic import LineReceiver
+from twisted.python import log
+from twisted.internet import reactor
+
+
+class CmdProtocol(LineReceiver):
+
+    delimiter = '\n'
+
+    def __init__(self):
+        self.client_ip = None
+
+    def connectionMade(self):
+        log.msg(self.transport)
+        self.client_ip = self.transport.getPeer().host
+        log.msg("Client connection from %s" % self.client_ip)
+        if len(self.factory.clients) >= self.factory.clients_max:
+            log.msg("Too many connections. bye !")
+            self.client_ip = None
+            self.transport.loseConnection()
+        else:
+            self.factory.clients.append(self.client_ip)
+            self.transport.write("test")
+            log.msg("Current client number is %d" % len(self.factory.clients))
+
+    def connectionLost(self, reason):
+        log.msg('Lost client connection.  Reason: %s' % reason)
+        if self.client_ip:
+            self.factory.clients.remove(self.client_ip)
+            log.msg("Current client number is %d" % len(self.factory.clients))
+
+    def lineReceived(self, line):
+        log.msg('Receive cmd from %s : %s' % (self.client_ip, line))
+
+
+class ServerPool(ServerFactory):
+
+    protocol = CmdProtocol
+
+    def __init__(self, clients_max=500):
+        self.clients_max = clients_max
+        self.clients = []
+
+
+def start():
+    log.startLogging(sys.stdout)
+    reactor.listenTCP(6888, ServerPool())
+    reactor.run()
